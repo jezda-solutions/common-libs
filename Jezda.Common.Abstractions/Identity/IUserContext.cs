a1056affe03f40c295ecb5a56c260f77be6e5e33
Zoran Jezdimirovic <zoran.jezdimirovic@gmail.com>
2025-07-08 16:59:16 +0200

    Init work

--- Jezda.Common.Abstractions/Identity/IUserContext.cs
+++ Jezda.Common.Abstractions/Identity/IUserContext.cs
@@ -6,32 +6,42 @@ namespace Jezda.Common.Abstractions.Identity;
 public interface IUserContext
 {
     /// <summary>
-    /// Id korisnikove organizacije
-    /// </summary>
-    Guid OrganisationId { get; }
-
-    /// <summary>
-    /// Korisnicki ID
+    /// Current user id.
     /// </summary>
     Guid UserId { get; }
 
     /// <summary>
+    /// Current user email.
+    /// </summary>
+    string Email { get; }
+
+    /// <summary>
+    /// Current user name.
+    /// </summary>
+    string UserName { get; }
+
+    /// <summary>
+    /// Id korisnikove organizacije
+    /// </summary>
+    Guid? CurrentOrganisationId { get; }
+
+    /// <summary>
     /// Lista korisnikovih rola
     /// </summary>
-    IEnumerable<string> UserRoles { get; }
-
-    // TODO: we should change the types as some user claims may not be Guid?
-    /// <summary>
-    /// Dictionary na koji mapiramo sve relevantne claimove korisnika
-    /// u ovom slucaju su to claimovi vezani samo za HRMS
-    /// </summary>
-    Dictionary<string, IEnumerable<Guid?>> UserClaims { get; }
+    IReadOnlyList<string> Roles { get; }
 
     /// <summary>
-    /// Ceo key value pair predstavlja claim.
-    /// String je type i u ovom slucaju je to HRMS.Claims.User
-    /// Lista guidova predstavlja sve organizacije koje su vezane za ovaj claim, te kasnije na osnovu
-    /// toga izvlacimo trenutnu organizaciju
+    /// Lista korisnikovih permisija
     /// </summary>
-    KeyValuePair<string, IEnumerable<Guid?>> CurrentOrganisation { get; }
+    IReadOnlyList<string> Permissions { get; }
+
+    // Fast checks
+    bool IsAuthenticated { get; }
+    bool IsAdmin { get; }
+    bool IsSupport { get; }
+
+    // Claim access (optional)
+    string? GetClaim(string type);
+
+    IEnumerable<string> GetClaims(string type);
 }
---
+++ Jezda.Common.Helpers/Identity/BaseUserContext.cs
@@ -0,0 +1,42 @@
+﻿using Jezda.Common.Abstractions.Identity;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Jezda.Common.Helpers.Identity;
+
+public abstract class BaseUserContext(IHttpContextAccessor accessor) : IUserContext
+{
+    private readonly IHttpContextAccessor _accessor = accessor;
+
+    protected ClaimsPrincipal User => _accessor.HttpContext?.User ?? new ClaimsPrincipal();
+
+    public Guid UserId => Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out var id)
+        ? id
+        : Guid.Empty;
+
+    public string Email => User.FindFirstValue(ClaimTypes.Email) ?? string.Empty;
+
+    public string UserName => User.FindFirstValue(ClaimTypes.Name) ?? string.Empty;
+
+    public Guid? CurrentOrganisationId => Guid.TryParse(User.FindFirstValue("current_org_id"), out var orgId)
+        ? orgId
+        : null;
+
+    public IReadOnlyList<string> Roles => [.. User.FindAll(ClaimTypes.Role).Select(x => x.Value).Distinct()];
+
+    public IReadOnlyList<string> Permissions => [.. User.FindAll("permissions").Select(x => x.Value).Distinct()];
+
+    public bool IsAuthenticated => User.Identity?.IsAuthenticated ?? false;
+
+    public bool IsAdmin => Roles.Contains("Nexus.Admin") || Roles.Contains("Retail.Admin");
+
+    public bool IsSupport => Roles.Contains("Nexus.Support");
+
+    public string? GetClaim(string type) => User.FindFirstValue(type);
+
+    public IEnumerable<string> GetClaims(string type) =>
+        User.FindAll(type).Select(c => c.Value);
+}
