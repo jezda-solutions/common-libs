7724d79fbcfa53b4558a48ebff33e093f35e7e01
Zoran Jezdimirovic <zoran.jezdimirovic@gmail.com>
2025-07-08 23:13:45 +0200

    User context and repository updates (#11)
    * Init work
    
    * Permission provider and naming
    
    * Update generics
    
    * Permissions are cached on server side
    
    * Remove them from interface

--- Jezda.Common.Abstractions/Identity/IUserContext.cs
+++ Jezda.Common.Abstractions/Identity/IUserContext.cs
@@ -6,32 +6,37 @@ namespace Jezda.Common.Abstractions.Identity;
 public interface IUserContext
 {
     /// <summary>
-    /// Id korisnikove organizacije
-    /// </summary>
-    Guid OrganisationId { get; }
-
-    /// <summary>
-    /// Korisnicki ID
+    /// Current user id.
     /// </summary>
     Guid UserId { get; }
 
     /// <summary>
+    /// Current user email.
+    /// </summary>
+    string Email { get; }
+
+    /// <summary>
+    /// Current user name.
+    /// </summary>
+    string UserName { get; }
+
+    /// <summary>
+    /// Id korisnikove organizacije
+    /// </summary>
+    Guid? CurrentOrganisationId { get; }
+
+    /// <summary>
     /// Lista korisnikovih rola
     /// </summary>
-    IEnumerable<string> UserRoles { get; }
+    IReadOnlyList<string> Roles { get; }
 
-    // TODO: we should change the types as some user claims may not be Guid?
-    /// <summary>
-    /// Dictionary na koji mapiramo sve relevantne claimove korisnika
-    /// u ovom slucaju su to claimovi vezani samo za HRMS
-    /// </summary>
-    Dictionary<string, IEnumerable<Guid?>> UserClaims { get; }
+    // Fast checks
+    bool IsAuthenticated { get; }
+    bool IsAdmin { get; }
+    bool IsSupport { get; }
 
-    /// <summary>
-    /// Ceo key value pair predstavlja claim.
-    /// String je type i u ovom slucaju je to HRMS.Claims.User
-    /// Lista guidova predstavlja sve organizacije koje su vezane za ovaj claim, te kasnije na osnovu
-    /// toga izvlacimo trenutnu organizaciju
-    /// </summary>
-    KeyValuePair<string, IEnumerable<Guid?>> CurrentOrganisation { get; }
+    // Claim access (optional)
+    string? GetClaim(string type);
+
+    IEnumerable<string> GetClaims(string type);
 }
