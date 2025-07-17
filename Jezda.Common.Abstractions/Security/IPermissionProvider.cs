b5ea945eaa1ee9989f0c4b718630c37845dbdb07
Zoran Jezdimirovic <zoran.jezdimirovic@gmail.com>
2025-07-17 09:10:02 +0200

    Init work

--- Jezda.Common.Abstractions/Security/IPermissionProvider.cs
+++ Jezda.Common.Abstractions/Security/IPermissionProvider.cs
@@ -17,7 +17,21 @@ public interface IPermissionProvider
     /// <param name="organisationId"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    Task<List<string>> GetPermissionsAsync(Guid userId, Guid organisationId, CancellationToken cancellationToken = default);
+    Task<List<string>> GetPermissionsAsync(
+        Guid userId,
+        Guid organisationId,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Get permission mask for user.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="organisationId"></param>
+    /// <returns></returns>
+    Task<long?> GetPermissionMaskAsync(
+        Guid userId,
+        Guid organisationId,
+        CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Has permission for user in organisation.
@@ -26,7 +40,11 @@ public interface IPermissionProvider
     /// <param name="organisationId"></param>
     /// <param name="permission"></param>
     /// <returns></returns>
-    Task<bool> HasPermissionAsync(Guid userId, Guid organisationId, string permission);
+    Task<bool> HasPermissionAsync(
+        Guid userId,
+        Guid organisationId,
+        string permission,
+        CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Set permission mask for user in organisation.
@@ -36,15 +54,12 @@ public interface IPermissionProvider
     /// <param name="permissionMask"></param>
     /// <param name="expiration"></param>
     /// <returns></returns>
-    Task SetPermissionMaskAsync(Guid userId, Guid organisationId, long permissionMask, TimeSpan expiration);
-
-    /// <summary>
-    /// Get permission mask for user.
-    /// </summary>
-    /// <param name="userId"></param>
-    /// <param name="organisationId"></param>
-    /// <returns></returns>
-    Task<long?> GetPermissionMaskAsync(Guid userId, Guid organisationId);
+    Task SetPermissionMaskAsync(
+        Guid userId,
+        Guid organisationId,
+        long permissionMask,
+        TimeSpan expiration,
+        CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Remove permission mask for user.
@@ -52,13 +67,18 @@ public interface IPermissionProvider
     /// <param name="userId"></param>
     /// <param name="organisationId"></param>
     /// <returns></returns>
-    Task RemovePermissionMaskAsync(Guid userId, Guid organisationId);
+    Task RemovePermissionMaskAsync(
+        Guid userId,
+        Guid organisationId,
+        CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Remove all permissions for user.
     /// </summary>
     /// <param name="userId"></param>
     /// <returns></returns>
-    Task RemoveAllUserPermissionsAsync(Guid userId);
+    Task RemoveAllUserPermissionsAsync(
+        Guid userId,
+        CancellationToken cancellationToken = default);
 }
 
