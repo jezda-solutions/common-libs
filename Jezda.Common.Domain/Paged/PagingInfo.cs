e97ab23bcb09fae6ccef2c101f7ecdbdb455e0b0
Aleksandar Knežević <aleksandarknezevicc04@gmail.com>
2025-08-10 10:56:33 +0200

    Paging info adjusted for all projects so that they can use fastep and normal asp net core

--- Jezda.Common.Domain/Paged/PagingInfo.cs
+++ Jezda.Common.Domain/Paged/PagingInfo.cs
@@ -7,34 +7,34 @@ namespace Jezda.Common.Domain.Paged;
 public class PagingInfo
 {
     [QueryParam, BindFrom("current_page")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "current_page")]
     public int CurrentPage { get; set; } = 1;
 
     [QueryParam, BindFrom("page_size")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "page_size")]
     public int PageSize { get; set; } = 10;
 
     [QueryParam, BindFrom("sort_column")]
-    public string SortColumn { get; set; } = default!;
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "sort_column")]
+    public string? SortColumn { get; set; }
 
     [QueryParam, BindFrom("sort_descending")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "sort_descending")]
     public bool SortDescending { get; set; } = false;
 
     [QueryParam, BindFrom("search_term")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "search_term")]
     public string? SearchTermJson { get; set; }
 
-    /// <summary>
-    /// Gets or sets the global search query used to filter results.
-    /// Responsible for filtering through all columns in the table
-    /// </summary>
     [QueryParam, BindFrom("global_search")]
-    public string? GlobalSearch { get; set; } = default!;
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "global_search")]
+    public string? GlobalSearch { get; set; } = string.Empty;
 
-    /// <summary>
-    /// Responsible for searching through one column (key in dictionary)
-    /// by value of the dictionary row.
-    /// </summary>
-    public Dictionary<string, string>? SearchTerm =>
-        JsonSerializer.Deserialize<Dictionary<string, string>>(SearchTermJson ?? "{}");
+    public Dictionary<string, string> SearchTerm =>
+        JsonSerializer.Deserialize<Dictionary<string, string>>(SearchTermJson ?? "{}") ?? [];
 
+    [QueryParam, BindFrom("total_count")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "total_count")]
     public int TotalCount { get; set; } = 0;
 
     public int TotalPages => GetTotalPages(TotalCount);
