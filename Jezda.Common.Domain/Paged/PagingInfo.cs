0a32d0def153834ae1de1673691d59d0a24210cc
Sale <149781566+AleksandarKnezevicSale@users.noreply.github.com>
2025-08-10 11:14:11 +0200

    v1.0.16
    * #30 Paging Info and Paging Info Request Merge
    
    * #34 Nuget package update over all projects
    
    * #37 snake case paging info (#38)
    
    * #32 v1.0.13
    
    * v1.0.14 - Nuget packages update
    
    * #30 Paging Info and Paging Info Request Merge
    
    * #34 Nuget package update over all projects
    
    * Snake case for paging info
    
    * Package updates up to date
    
    * #41 Paging info adjusted for all projects so that they can use fastep and normal asp net core 
    
    * #32 v1.0.13
    
    * v1.0.14 - Nuget packages update
    
    * #30 Paging Info and Paging Info Request Merge
    
    * #34 Nuget package update over all projects
    
    * v1.0.15
    
    * #30 Paging Info and Paging Info Request Merge
    
    * #34 Nuget package update over all projects
    
    * #37 snake case paging info (#38)
    
    * #32 v1.0.13
    
    * v1.0.14 - Nuget packages update
    
    * #30 Paging Info and Paging Info Request Merge
    
    * #34 Nuget package update over all projects
    
    * Snake case for paging info
    
    * Package updates up to date
    
    * Paging info adjusted for all projects so that they can use fastep and normal asp net core
    
    * Removed not needed query declarations
    
    * Added comments

--- Jezda.Common.Domain/Paged/PagingInfo.cs
+++ Jezda.Common.Domain/Paged/PagingInfo.cs
@@ -6,19 +6,39 @@ namespace Jezda.Common.Domain.Paged;
 
 public class PagingInfo
 {
+    /// <summary>
+    /// Current page number in the paginated result set.
+    /// </summary>
     [QueryParam, BindFrom("current_page")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "current_page")]
     public int CurrentPage { get; set; } = 1;
 
+    /// <summary>
+    /// Specifies the number of items to return per page.
+    /// </summary>
     [QueryParam, BindFrom("page_size")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "page_size")]
     public int PageSize { get; set; } = 10;
 
+    /// <summary>
+    /// Column name to sort by.
+    /// </summary>
     [QueryParam, BindFrom("sort_column")]
-    public string SortColumn { get; set; } = default!;
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "sort_column")]
+    public string? SortColumn { get; set; }
 
+    /// <summary>
+    /// Directs the sorting order of the column specified in SortColumn.
+    /// </summary>
     [QueryParam, BindFrom("sort_descending")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "sort_descending")]
     public bool SortDescending { get; set; } = false;
 
+    /// <summary>
+    /// Responsible for searching through one column (key in dictionary)
+    /// by value of the dictionary row.
     [QueryParam, BindFrom("search_term")]
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "search_term")]
     public string? SearchTermJson { get; set; }
 
     /// <summary>
@@ -26,19 +46,30 @@ public class PagingInfo
     /// Responsible for filtering through all columns in the table
     /// </summary>
     [QueryParam, BindFrom("global_search")]
-    public string? GlobalSearch { get; set; } = default!;
+    [Microsoft.AspNetCore.Mvc.FromQuery(Name = "global_search")]
+    public string? GlobalSearch { get; set; } = string.Empty;
 
     /// <summary>
-    /// Responsible for searching through one column (key in dictionary)
-    /// by value of the dictionary row.
+    /// Parses the SearchTermJson into a dictionary. (key: column name, value: search term)
     /// </summary>
-    public Dictionary<string, string>? SearchTerm =>
-        JsonSerializer.Deserialize<Dictionary<string, string>>(SearchTermJson ?? "{}");
+    public Dictionary<string, string> SearchTerm =>
+        JsonSerializer.Deserialize<Dictionary<string, string>>(SearchTermJson ?? "{}") ?? [];
 
+    /// <summary>
+    /// Total number of items in the result set. This is used to calculate the total number of pages.
+    /// </summary>
     public int TotalCount { get; set; } = 0;
 
+    /// <summary>
+    /// Total number of pages in the result set.
+    /// </summary>
     public int TotalPages => GetTotalPages(TotalCount);
 
+    /// <summary>
+    /// Calculates the total number of pages based on the total count of items and the page size.
+    /// </summary>
+    /// <param name="totalCount"></param>
+    /// <returns></returns>
     public int GetTotalPages(int totalCount)
     {
         var pageSize = PageSize;
