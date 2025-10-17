983540cdaf50601643fd165c01054455c8ea1710
Sale <149781566+AleksandarKnezevicSale@users.noreply.github.com>
2025-10-17 16:13:21 +0200

    Interface with correct properties (#83)

--- Jezda.Common.Domain/Entities/Interfaces/IAuditEntity.cs
+++ Jezda.Common.Domain/Entities/Interfaces/IAuditEntity.cs
@@ -1,3 +1,16 @@
-﻿namespace Jezda.Common.Domain.Entities.Interfaces;
+﻿using System;
 
-public interface IAuditEntity;
+namespace Jezda.Common.Domain.Entities.Interfaces;
+
+public interface IAuditEntity
+{
+    Guid CreatedBy { get; set; }
+    
+    DateTimeOffset CreatedOn { get; set; }
+    
+    Guid? UpdatedBy { get; set; }
+    
+    DateTimeOffset? UpdatedOn { get; set; }
+    
+    bool IsDeleted { get; set; }
+}
