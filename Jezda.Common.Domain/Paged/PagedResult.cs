6cae55e3c1c18d7eae4294f05a801844a23c6082
Aleksandar Knežević <aleksandarknezevicc04@gmail.com>
2025-11-27 16:38:17 +0100

    Paged Result

---
+++ Jezda.Common.Domain/Paged/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Jezda.Common.Domain.Paged;
+
+public sealed record PagedResult<T>(
+    List<T> Items, 
+    int TotalCount, 
+    int CurrentPage, 
+    int PageSize
+);
